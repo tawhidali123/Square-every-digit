8a6f6f293aa620d6f21a1cb8d9df631991b8308d
Tawhid Ali <tawhidali126@gmail.com>
2020-01-14 03:10:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//  you are asked to square every digit of a number.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+
+function squareDigits(num){
+  let splitUp = num.toString().split('')
+  let empty = []
+  for(num of splitUp){
+    empty.push(num ** 2);
+  }
+  return parseInt(empty.join(""))
+}
+
+Test.assertEquals(squareDigits(9119), 811181);
